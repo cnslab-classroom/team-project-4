e88f7290b5e1c6db080360c6bfc7a379c26ad941
sunghoonhan-hh <hansoung2002@naver.com>
2024-11-18 14:26:12 +0900

    thread modified

--- ExpiryTimer.java
+++ ExpiryTimer.java
@@ -11,19 +11,25 @@ public class ExpiryTimer implements Runnable {
 
     @Override
     public void run() {
+        long startTime = System.currentTimeMillis();
         while (running) {
-            try {
-                Thread.sleep(1000); // 1초 대기
-
+                // 1초가 지날 때까지 대기
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - startTime >= 1000) { // 1000밀리초 == 1초
+                System.out.println("1초가 지났습니다!"); // 30분 대기
+                int i=0;
                 refrigerator.minusExpiry();
                 System.out.println("1 second passed, expiry dates updated.");
                 refrigerator.print();
-
-            }
-
-            catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.out.println("Timer interrupted");
+                while(refrigerator.get(0).getExpiry()==0){
+                    System.out.println(refrigerator.get(0)+" is elemenated");
+                    refrigerator.pop();
+                }                    
+                while(refrigerator.get(i).getExpiry()<3){
+                        System.out.println(refrigerator.get(i).getName()+": 남은 유통기한 ("+refrigerator.get(i).getExpiry()+") ");
+                    i++;
+                }
+                currentTime=startTime;               
             }
         }
     }
